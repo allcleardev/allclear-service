5c6dde88d2805d66a2962260a64cf2ed4adede61
David Small <smalleyd@MacBook-Air.local>
2020-03-25 17:16:55 -0400

    Added SessionValue.

---
+++ platform-model/src/app/allclear/platform/value/SessionValue.java
@@ -0,0 +1,96 @@
+package app.allclear.platform.value;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.UUID;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import app.allclear.platform.model.StartRequest;
+
+/** Value object that represents an authenticated session to interact
+ *  with the server-side resources.
+ *
+ * @author smalleyd
+ * @versiion
+ *
+ */
+public class SessionValue implements Serializable
+{
+	private static final long serialVersionUID = 1L;
+
+	public static final long DURATION_SHORT = 30L * 60L * 1000L;
+	public static final long DURATION_LONG = 30L * 24L * 60L * 60L * 1000L;
+
+	// Members
+	public final String id;
+	public final boolean rememberMe;
+	public final long duration;	// Millseconds
+	public final PeopleValue person;
+	public final StartRequest registration;
+	public final Date expiresAt;
+	public final Date lastAccessedAt;
+	public final Date createdAt;
+
+	// Mutators
+	public SessionValue accessed()
+	{
+		lastAccessedAt.setTime(System.currentTimeMillis());
+
+		return this;
+	}
+
+	public SessionValue(final StartRequest registration)
+	{
+		this(
+			false,
+			DURATION_SHORT,
+			null,
+			registration,
+			new Date());
+	}
+
+	public SessionValue(final boolean rememberMe, final PeopleValue person)
+	{
+		this(rememberMe,
+			rememberMe ? DURATION_LONG : DURATION_SHORT,
+			person,
+			null,
+			new Date());
+	}
+
+	public SessionValue(final boolean rememberMe,
+		final long duration,
+		final PeopleValue person,
+		final StartRequest registration,
+		final Date createdAt)
+	{
+		this(UUID.randomUUID().toString(),
+			rememberMe,
+			duration,
+			person,
+			registration,
+			new Date(createdAt.getTime() + duration),
+			createdAt,
+			createdAt);
+	}
+
+	public SessionValue(@JsonProperty("id") final String id,
+		@JsonProperty("rememberMe") final boolean rememberMe,
+		@JsonProperty("duration") final long duration,
+		@JsonProperty("person") final PeopleValue person,
+		@JsonProperty("registration") final StartRequest registration,
+		@JsonProperty("expiresAt") final Date expiresAt,
+		@JsonProperty("lastAccessedAt") final Date lastAccessedAt,
+		@JsonProperty("createdAt") final Date createdAt)
+	{
+		this.id = id;
+		this.rememberMe = rememberMe;
+		this.duration = duration;
+		this.person = person;
+		this.registration = registration;
+		this.expiresAt = expiresAt;
+		this.lastAccessedAt = lastAccessedAt;
+		this.createdAt = createdAt;
+	}
+}
---
+++ platform-model/test/app/allclear/platform/value/SessionValueTest.java
@@ -0,0 +1,84 @@
+package app.allclear.platform.value;
+
+import static org.fest.assertions.api.Assertions.assertThat;
+
+import java.util.Date;
+
+import org.junit.jupiter.api.*;
+
+import app.allclear.common.ThreadUtils;
+import app.allclear.platform.model.StartRequest;
+
+/** Unit test class that verifies the SessionValue POJO.
+ * 
+ * @author smalleyd
+ * @version 1.0.0
+ * @since 3/25/2020
+ *
+ */
+
+public class SessionValueTest
+{
+	@Test
+	public void create_people()
+	{
+		var o = new SessionValue(false, new PeopleValue("stevie", "888-555-0001", true));
+		assertThat(o.id).as("Check ID").hasSize(36);
+		Assertions.assertFalse(o.rememberMe, "Check rememberMe");
+		Assertions.assertEquals(SessionValue.DURATION_SHORT, o.duration, "Check duration");
+		Assertions.assertNotNull(o.person, "Check person");
+		Assertions.assertEquals("stevie", o.person.name, "Check person.name");
+		Assertions.assertEquals("888-555-0001", o.person.phone, "Check person.phone");
+		Assertions.assertTrue(o.person.active, "Check person.active");
+		Assertions.assertNull(o.registration, "Check registration");
+		assertThat(o.expiresAt).as("Check expiresAt").isCloseTo(new Date(System.currentTimeMillis() + SessionValue.DURATION_SHORT), 100L);
+		assertThat(o.lastAccessedAt).as("Check lastAccessedAt").isCloseTo(new Date(), 100L).isEqualTo(o.createdAt);
+
+		ThreadUtils.sleep(2000L);
+
+		var lastAccessedAt = new Date(o.lastAccessedAt.getTime());
+		assertThat(o.accessed().lastAccessedAt).as("Check accessed").isAfter(lastAccessedAt);
+	}
+
+	@Test
+	public void create_people_rememberMe()
+	{
+		var o = new SessionValue(true, new PeopleValue("moira", "888-555-0002", false));
+		assertThat(o.id).as("Check ID").hasSize(36);
+		Assertions.assertTrue(o.rememberMe, "Check rememberMe");
+		Assertions.assertEquals(SessionValue.DURATION_LONG, o.duration, "Check duration");
+		Assertions.assertNotNull(o.person, "Check person");
+		Assertions.assertEquals("moira", o.person.name, "Check person.name");
+		Assertions.assertEquals("888-555-0002", o.person.phone, "Check person.phone");
+		Assertions.assertFalse(o.person.active, "Check person.active");
+		Assertions.assertNull(o.registration, "Check registration");
+		assertThat(o.expiresAt).as("Check expiresAt").isCloseTo(new Date(System.currentTimeMillis() + SessionValue.DURATION_LONG), 100L);
+		assertThat(o.lastAccessedAt).as("Check lastAccessedAt").isCloseTo(new Date(), 100L).isEqualTo(o.createdAt);
+
+		ThreadUtils.sleep(2000L);
+
+		var lastAccessedAt = new Date(o.lastAccessedAt.getTime());
+		assertThat(o.accessed().lastAccessedAt).as("Check accessed").isAfter(lastAccessedAt);
+	}
+
+	@Test
+	public void create_registration()
+	{
+		var o = new SessionValue(new StartRequest("888-555-0003", true, true));
+		assertThat(o.id).as("Check ID").hasSize(36);
+		Assertions.assertFalse(o.rememberMe, "Check rememberMe");
+		Assertions.assertEquals(SessionValue.DURATION_SHORT, o.duration, "Check duration");
+		Assertions.assertNull(o.person, "Check person");
+		Assertions.assertNotNull(o.registration, "Check registration");
+		Assertions.assertEquals("888-555-0003", o.registration.phone, "Check registration.phone");
+		Assertions.assertTrue(o.registration.beenTested, "Check registration.beenTested");
+		Assertions.assertTrue(o.registration.haveSymptoms, "Check registration.haveSymptoms");
+		assertThat(o.expiresAt).as("Check expiresAt").isCloseTo(new Date(System.currentTimeMillis() + SessionValue.DURATION_SHORT), 100L);
+		assertThat(o.lastAccessedAt).as("Check lastAccessedAt").isCloseTo(new Date(), 100L).isEqualTo(o.createdAt);
+
+		ThreadUtils.sleep(2000L);
+
+		var lastAccessedAt = new Date(o.lastAccessedAt.getTime());
+		assertThat(o.accessed().lastAccessedAt).as("Check accessed").isAfter(lastAccessedAt);
+	}
+}
