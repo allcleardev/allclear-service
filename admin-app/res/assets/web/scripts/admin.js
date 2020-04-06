1ed6189f0c017b30866e8b60b25e63d95eb59b5f
David Small <smalleyd@ip-10-0-1-196.ec2.internal>
2020-04-06 18:49:57 -0400

    Modified FacilitiesHandler.SEARCH: fixed hasTestCriteriaId.

--- admin-app/res/assets/web/scripts/admin.js
+++ admin-app/res/assets/web/scripts/admin.js
@@ -186,7 +186,7 @@ var FacilitiesHandler = new ListTemplate({
 		      new ListField('referralRequired', 'Referral Required?', false, 'yesNoOptions', undefined, 'No Search'),
 		      new ListField('testCriteriaId', 'Testing Criteria', false, 'testCriteria', undefined, 'No Search'),
 		      new ListField('notTestCriteriaId', 'Exclude Testing Criteria', false, 'testCriteria', undefined, 'No Search'),
-		      new ListField('hasTestCriteria', 'Has Test Criteria', false, 'yesNoOptions', undefined, 'No Search'),
+		      new ListField('hasTestCriteriaId', 'Has Test Criteria', false, 'yesNoOptions', undefined, 'No Search'),
 		      new EditField('otherTestCriteria', 'Other Test Criteria', false, false, 60, 5),
 		      new ListField('hasOtherTestCriteria', 'Has Other Test Criteria', false, 'yesNoOptions', undefined, 'No Search'),
 		      new EditField('testsPerDay', 'Tests per Day', false, false, 10, 10),
