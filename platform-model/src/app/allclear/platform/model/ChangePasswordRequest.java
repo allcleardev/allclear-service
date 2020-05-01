6ee8ffd70e1db1e6a1161fc1d85f124040f69fb4
David Small <bokenrunner@gmail.com>
2020-05-01 12:16:35 -0400

    Modified Admin components: added Change Password operation.

---
+++ platform-model/src/app/allclear/platform/model/ChangePasswordRequest.java
@@ -0,0 +1,33 @@
+package app.allclear.platform.model;
+
+import java.io.Serializable;
+
+import org.apache.commons.lang3.StringUtils;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+/** Value object that represents a request to the current user's password.
+ * 
+ * @author smalleyd
+ * @version 1.1.22
+ * @since 5/1/2020
+ *
+ */
+
+public class ChangePasswordRequest implements Serializable
+{
+	private static final long serialVersionUID = 1L;
+
+	public final String currentPassword;
+	public final String newPassword;
+	public final String confirmPassword;
+
+	public ChangePasswordRequest(@JsonProperty("currentPassword") final String currentPassword,
+		@JsonProperty("newPassword") final String newPassword,
+		@JsonProperty("confirmPassword") final String confirmPassword)
+	{
+		this.currentPassword = StringUtils.trimToNull(currentPassword);
+		this.newPassword = StringUtils.trimToNull(newPassword);
+		this.confirmPassword = StringUtils.trimToNull(confirmPassword);
+	}
+}
